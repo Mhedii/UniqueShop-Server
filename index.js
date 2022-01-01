88c39b5ba54b2b14feb2ae29c55d4104378b503d
zahidhasan2806 <jahidh714@gmail.comm>
2022-01-01 21:53:08 +0600

    fix query by id prob

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
