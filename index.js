d503b2fd495dc544aaae0b22900cb00f99e1bd52
zahidhasan2806 <jahidh714@gmail.comm>
2022-01-03 16:01:06 +0600

    add product order add delete and update api create

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ const ObjectId = require('mongodb').ObjectId;
 
 require('dotenv').config()
 const port = process.env.PORT || 5000
-
+const fileUpload = require('express-fileupload')
 
 app.use(cors())
 app.use(express.json());
+app.use(fileUpload());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xohwd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -23,7 +24,31 @@ async function run() {
         const database = client.db("uniqueShop");
         const productsCollection = database.collection("products");
         const usersCollection = database.collection('users');
+        const ordersCollection = database.collection("orders");
 
+
+
+        //POST API for  new products
+        app.post("/products", async (req, res) => {
+            const title = req.body.title;
+            const Categories = req.body.Categories;
+            const desc1 = req.body.desc1;
+            const desc2 = req.body.desc2;
+            const desc3 = req.body.desc3;
+            const desc4 = req.body.desc4;
+            const price = req.body.price;
+            const image = req.files.image;
+
+            const imageData = image.data;
+            const encodedData = imageData.toString('base64')
+            const imgBuffer = Buffer.from(encodedData, 'base64')
+            const data = {
+                title, desc3, desc4, price, desc1, desc2, Categories,
+                image: imgBuffer
+            }
+            const result = await productsCollection.insertOne(data);
+            res.json(result);
+        })
 
         //GET API for all the products\ showing UI
         app.get("/products", async (req, res) => {
@@ -53,6 +78,45 @@ async function run() {
             const updateDoc = { $set: user };
             const result = await usersCollection.updateOne(filter, updateDoc, options);
             res.json(result);
+        });
+
+        //POST API for Products order
+        app.post('/orders', async (req, res) => {
+            const orders = await ordersCollection.insertOne(req.body);
+            res.json(orders);
+        });
+
+        //GET API-orders 
+        app.get('/orders', async (req, res) => {
+            const orders = await ordersCollection.find({}).toArray();
+            res.send(orders);
+        });
+        app.get("/orders/:email", async (req, res) => {
+            const result = await ordersCollection.find({
+                user_email: req.params.email,
+            }).toArray();
+            res.send(result);
+        });
+        //Delete API- delete order
+        app.delete('/orders/:id', async (req, res) => {
+            const deletedOrder = await ordersCollection.deleteOne({ _id: ObjectId(req.params.id) });
+            res.json(deletedOrder)
+        });
+
+        //Update order status api
+        app.put('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const updateStatus = req.body;
+            const options = { upsert: true }
+            const updateDoc = {
+                $set: {
+                    status: updateStatus
+                }
+            };
+            const result = await ordersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+
         })
 
     } finally {
